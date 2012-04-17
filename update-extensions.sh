1ea6ad07ddc71f9ebf722dd8c3a2874d06314f36
Aaron Schulz <aschulz@wikimedia.org>
2012-04-17 01:56:47 -0700

    Made update-extensions call git-review automatically so the first commits will always have change-Ids.
    Change 1: fail out if git-review is not present
    
    Change-Id: I605c5e903960bcf67a316054eac0a90b4d88908a

--- update-extensions.sh
+++ update-extensions.sh
@@ -3,6 +3,12 @@
 # Config
 basePath="mediawiki/extensions/"
 
+# Check for git-review
+if [ -z $(which git-review) ]; then
+	echo "git-review not detected. Aborting."
+	exit 1
+fi
+
 # Script to clone any missing extensions and updates the others
 ssh -p 29418 gerrit.wikimedia.org gerrit ls-projects | grep "^${basePath}" | sed "s,${basePath},," | while read PROJECT
 do
@@ -10,6 +16,7 @@ do
 	# Clone projects that don't exist here...
 	if [ ! -d "${PROJECT}" ]; then
 		git clone "ssh://gerrit.wikimedia.org:29418/${basePath}${PROJECT}.git" "${PROJECT}"
+		git-review -s
 	# Update any existing project...
 	else
 		cd "${PROJECT}"
