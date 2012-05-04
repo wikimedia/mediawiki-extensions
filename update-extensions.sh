25850933662b978f64a3453ba636e5517cc5112f
Chad Horohoe <chadh@wikimedia.org>
2012-05-04 15:32:24 -0400

    Add all current extensions as .gitmodules to the extension repo
    - To clone all extensions once you've cloned this repo, do
      `git submodule init && git submodule update`
    - Gerrit 2.3 is supposed to take care of automatically updating
      the references when HEAD shifts on the subproject, so don't
      worry about updating those yourself
    - Deleted update-submodules.sh, fixed up update-extensions.sh
      to do what it should
    
    Patch set 2: Adjust submodules to HTTPS instead of SSH
    
    Change-Id: Ib87abfe6322d851ffd7b2402a1256e057bf76b82

--- update-extensions.sh
+++ update-extensions.sh
@@ -3,24 +3,13 @@
 # Config
 basePath="mediawiki/extensions/"
 
-# Check for git-review
-if [ -z $(which git-review) ]; then
-	echo "git-review not detected. Aborting."
-	exit 1
-fi
-
 # Script to clone any missing extensions and updates the others
 ssh -p 29418 gerrit.wikimedia.org gerrit ls-projects | grep "^${basePath}" | sed "s,${basePath},," | while read PROJECT
 do
 	echo "[${PROJECT}]:"
 	# Clone projects that don't exist here...
 	if [ ! -d "${PROJECT}" ]; then
-		git clone "ssh://gerrit.wikimedia.org:29418/${basePath}${PROJECT}.git" "${PROJECT}"
-		git-review -s
-	# Update any existing project...
-	else
-		cd "${PROJECT}"
-		git pull
-		cd "../"
+		git submodule add "https://gerrit.wikimedia.org/r/p/${basePath}${PROJECT}.git" "${PROJECT}"
+		echo "\tbranch = ." >> .gitmodules
 	fi
 done
